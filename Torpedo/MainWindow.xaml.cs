778585058d097a8a672424276c9aa361afe6e536
csabad1997 <csabad1997@gmail.com>
2022-05-26 13:12:30 +0200

    Initial commit

---
+++ Torpedo/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using BusinessLogicLayer;
+using DataContract;
+
+namespace Torpedo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
