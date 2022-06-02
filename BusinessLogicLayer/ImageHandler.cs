40d38399461c225efbe0842cfa8ba2e705c54fb1
csabad1997 <csabad1997@gmail.com>
2022-06-02 08:30:31 +0200

    ImageHandler, GameFlowDirector, WPF ablakok hozzáadva

---
+++ BusinessLogicLayer/ImageHandler.cs
@@ -0,0 +1,69 @@
+﻿using DataContract;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace BusinessLogicLayer
+{
+    public static class ImageHandler
+    {
+        private static Pen blackPen = new Pen(Color.Black, 1);
+        private static SolidBrush redBrush = new SolidBrush(Color.Red);
+        private static SolidBrush whiteBrush = new SolidBrush(Color.White);
+        private static SolidBrush bgBrush = new SolidBrush(Color.FromArgb(55, 55, 200));
+        private static SolidBrush greyBrush = new SolidBrush(Color.LightGray);
+        private static SolidBrush shipBrush = new SolidBrush(Color.DarkGray);
+
+        public static Bitmap DrawMap(GameBoard gameBoard, Bitmap bmp = null, bool isOpponentTable = false)
+        {
+            if (bmp == null)
+            {
+                bmp = new Bitmap(311, 311);
+            }
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.FillRectangle(bgBrush, 0, 0, bmp.Width, bmp.Height);
+                for (int i = 0; i < gameBoard.Matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < gameBoard.Matrix.GetLength(1); j++)
+                    {
+                        TableCell cell = gameBoard.Matrix[i, j];
+                        if (cell.isHovered)
+                        {
+                            g.FillRectangle(greyBrush, (i * 31) + 1, (j * 31) + 1, 30, 30);
+                        }
+                        else
+                        {
+                            switch (cell.state)
+                            {
+                                case TableCellSateEnum.EMPTY:
+                                    //g.FillRectangle(bgBrush, (i * 30) + 1, (j * 30) + 1, 30, 30);
+                                    break;
+                                case TableCellSateEnum.SHIP:
+                                    //if(!isOpponentTable)
+                                    g.FillRectangle(shipBrush, (i * 31) + 1, (j * 31) + 1, 30, 30);
+                                    break;
+                                case TableCellSateEnum.MISS:
+                                    g.FillRectangle(whiteBrush, (i * 31) + 1, (j * 31) + 1, 30, 30);
+                                    break;
+                                case TableCellSateEnum.HIT:
+                                    g.FillRectangle(redBrush, (i * 31) + 1, (j * 31) + 1, 30, 30);
+                                    break;
+                                default:
+                                    g.FillRectangle(bgBrush, (i * 31) + 1, (j * 31) + 1, 30, 30);
+                                    break;
+                            }
+                        }
+                    }
+                }
+                for (int i = 0; i < 11; i++)
+                {
+                    g.DrawLine(blackPen, 0, (i * 30) + i, bmp.Width, (i * 30) + i);
+                    g.DrawLine(blackPen, (i * 30) + i, 0, (i * 30) + i, bmp.Height);
+                }
+            }
+            return bmp;
+        }
+    }
+}
---
+++ Torpedo/GameFlowDirector.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataContract;
+
+namespace Torpedo
+{
+    public static class GameFlowDirector
+    {
+        public static bool IsServer { get; set; }
+        public static bool IsAiOpponent { get; set; }
+        public static string WinnerName { get; set; }
+        public static UserData User { get; set; }
+        public static UserData Opponent { get; set; }
+
+        public static GameFlowStepEnum GameFlowStep = GameFlowStepEnum.Login;
+        public static GameBoard GameBoard { get; set; }
+        public static GameBoard EnemyGameBoard { get; set; }
+
+        public static bool ServerTurn = false;
+
+        public static bool IsGameOver()
+        {
+            if (GameFlowStep == GameFlowStepEnum.GameOn)
+            {
+                int shipsFound = 0;
+                for (int i = 0; i < GameBoard.Matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < GameBoard.Matrix.GetLength(1); j++)
+                    {
+                        if (GameBoard.Matrix[i, j].state == TableCellSateEnum.SHIP)
+                        {
+                            shipsFound++;
+                        }
+                    }
+                }
+                if (shipsFound == 0)
+                {
+                    WinnerName = Opponent.UserName;
+                    StepForward();
+                    return true;
+                }
+                else
+                {
+                    shipsFound = 0;
+                    for (int i = 0; i < EnemyGameBoard.Matrix.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < EnemyGameBoard.Matrix.GetLength(1); j++)
+                        {
+                            if (EnemyGameBoard.Matrix[i, j].state == TableCellSateEnum.SHIP)
+                            {
+                                shipsFound++;
+                            }
+                        }
+                    }
+                    if (shipsFound == 0)
+                    {
+                        WinnerName = User.UserName;
+                        StepForward();
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static void StepForward()
+        {
+            if (GameFlowStep != GameFlowStepEnum.GameStop)
+                GameFlowStep++;
+        }
+        public static void StepBackward()
+        {
+            if (GameFlowStep != GameFlowStepEnum.Login)
+                GameFlowStep--;
+        }
+
+    }
+}
